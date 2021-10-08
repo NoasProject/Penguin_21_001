23aac38e377aab446359c9db0c2211e5938c0777
NoasProject <lowntry@gmail.com>
2021-10-09 01:46:18 +0900

    Update

--- front/src/main.js
+++ front/src/main.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
-import App from "./lib/App.vue";
-import Todo from "./lib/Todo.vue";
+import App from "./views/App.vue";
 import axios from "axios";
 import vueAxios from "vue-axios";
 // axios.defaults.baseURL = process.env.APP_API_ENDPOINT;
@@ -8,14 +7,6 @@ import vueAxios from "vue-axios";
 Vue.config.productionTip = false;
 
 Vue.use(vueAxios, axios);
-
-new Vue({
-  render: (h) => h(Todo),
-  data: {
-    newItem: "",
-    todos: []
-  }
-}).$mount("#todo");
 
 new Vue({
   render: (h) => h(App),
