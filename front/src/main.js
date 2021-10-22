e5cebd1ef958dcd98a1c8bb55274d5b393ad76ad
NoasProject <lowntry@gmail.com>
2021-10-23 06:12:56 +0900

    BootStrap

--- front/src/main.js
+++ front/src/main.js
@@ -3,10 +3,14 @@ import App from "./views/App.vue";
 import axios from "axios";
 import vueAxios from "vue-axios";
 // axios.defaults.baseURL = process.env.APP_API_ENDPOINT;
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false;
 
 Vue.use(vueAxios, axios);
+Vue.use(BootstrapVue)
 
 new Vue({
   render: (h) => h(App),
